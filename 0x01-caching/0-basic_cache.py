87f97bcebad6ca8671be63b1d6937ccb1143e630
MARWANANO32 <marwanano32@gmail.com>
2024-08-21 17:30:31 +0300

    MARWAN MOSTAFA

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,11 +7,16 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     '''basic caching system'''
     def put(self, key, item):
-        '''method that puts items in cache'''
+        '''method that puts items in cache
+        if cache_data is full, one item is removed
+        '''
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        '''method that gets items from cache'''
-
+        '''method that gets items from cache
+        if key is None or the key doesn't exist, return None
+        '''
+        if key is None or key not in self.cache_data:
+            return None
         return self.cache_data[key]
