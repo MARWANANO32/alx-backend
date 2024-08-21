837910bf8af984bf79f7b57552bee741f27d864f
MARWANANO32 <marwanano32@gmail.com>
2024-08-21 17:25:12 +0300

    MARWAN MOSTAFA

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,11 +7,13 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     '''basic caching system'''
     def put(self, key, item):
+        self.cache_data[key] = item
         '''method that puts items in cache'''
         if key and item is None:
             return
 
     def get(self, key):
+        self.cache_data.get(key)
         '''method that gets items from cache'''
         if key is None:
             return None
