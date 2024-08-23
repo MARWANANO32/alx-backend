3b5a12a9f7d2876cf0900ef1f292cefb6d197f0f
MARWANANO32 <marwanano32@gmail.com>
2024-08-23 17:42:57 +0300

    MARWAN MOSTAFA

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
+"""function named index_range that takes two integer arguments."""
 
-# function named index_range that takes two integer arguments.   
 
 def index_range(page: int, page_size: int) -> tuple:
     """The function should return a tuple of size 
     two containing a start index and an end index corresponding
       to the range of indexes to return in a list for those particular 
         pagination parameters."""
-    
+
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
