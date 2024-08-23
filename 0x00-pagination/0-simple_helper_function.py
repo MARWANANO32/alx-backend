e86f5aa50d063a6040f85a2872d275dd9254f75d
MARWANANO32 <marwanano32@gmail.com>
2024-08-23 17:44:00 +0300

    MARWAN MOSTAFA

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,7 +2,7 @@
 """function named index_range that takes two integer arguments."""
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> tuple[int, int]:
     """The function should return a tuple of size 
     two containing a start index and an end index corresponding
       to the range of indexes to return in a list for those particular 
