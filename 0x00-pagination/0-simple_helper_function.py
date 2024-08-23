de2b21dfb75c45d93f7ced23b01479e6332874b5
MARWANANO32 <marwanano32@gmail.com>
2024-08-23 17:46:28 +0300

    MARWAN MOSTAFA

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,5 +10,5 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
         pagination parameters."""
 
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = ((page - 1) * page_size + page_size)
     return (start_index, end_index)
